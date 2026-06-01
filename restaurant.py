9d48b54bd9335b5f6fc2577cd6d95960ca5ea7ae
vladiqua <vlagoleg@gmail.com>
2026-06-01 14:36:13 +0300

    Add restaurant.py solution for practice 13

---
+++ restaurant.py
@@ -0,0 +1,9 @@
+﻿# restaurant.py
+class Restaurant:
+    def __init__(self, name, cuisine, rating=0):
+        self.name = name
+        self.cuisine = cuisine
+        self.rating = rating
+    def describe(self):
+        print(f"{self.name}, {self.cuisine}, рейтинг {self.rating}")
+# и т.д. (вставьте полный код из предыдущего сообщения)
